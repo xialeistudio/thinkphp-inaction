1a3edf8952ec9cad1ebc6371af69ee6d5344c4f2
xialeistudio <xialeistudio@gmail.com>
2016-10-28 17:19:22 +0800

    用户主题管理完成

--- bbs/Application/Home/Controller/UserController.class.php
+++ bbs/Application/Home/Controller/UserController.class.php
@@ -7,6 +7,8 @@
  */
 namespace Home\Controller;
 
+use Common\Model\PostModel;
+use Common\Model\PostViewModel;
 use Common\Model\UserModel;
 use Think\Controller;
 use Think\Exception;
@@ -89,6 +91,87 @@ class UserController extends CommonController
             $user = $model->find($this->user['userId']);
             $this->assign('user', $user);
             $this->display();
+        }
+    }
+
+    /**
+     * 用户帖子列表
+     */
+    public function posts()
+    {
+        $this->checkLogin();
+        $model = new PostViewModel();
+        list($list, $page, $count) = $model->getList($this->user['userId']);
+        $this->assign('list', $list);
+        $this->assign('page', $page);
+        $this->assign('count', $count);
+        $this->display();
+    }
+
+    /**
+     * 编辑帖子
+     */
+    public function postEdit()
+    {
+        $this->checkLogin();
+        $id = I('id');
+        $model = new PostModel();
+        $post = $model->find($id);
+        if (empty($post)) {
+            $this->error('帖子不存在');
+        }
+        if ($post['userId'] != $this->user['userId']) {
+            $this->error('你无权编辑');
+        }
+        $this->assign('post', $post);
+        $this->display();
+    }
+
+    /**
+     * 处理帖子编辑
+     */
+    public function postDoPost()
+    {
+        $this->checkLogin();
+        try {
+            $id = I('id');
+            $title = I('title');
+            $content = I('content');
+            $data = array(
+                'title' => $title,
+                'content' => $content
+            );
+            $model = new PostModel();
+            if ($model->where(array('postId' => $id))->save($data) === false) {
+                throw new Exception('编辑失败');
+            }
+            $this->success('编辑成功', U('posts'));
+        } catch (Exception $e) {
+            $this->error($e->getMessage());
+        }
+    }
+
+    /**
+     * 删除帖子
+     */
+    public function postDelete()
+    {
+        $this->checkLogin();
+        $id = I('id');
+        $model = new PostModel();
+        $post = $model->find($id);
+        if (empty($post)) {
+            $this->error('帖子不存在');
+        }
+        if ($post['userId'] != $this->user['userId']) {
+            $this->error('你无权删除');
+        }
+
+        $result = $model->delete($id);
+        if ($result === false) {
+            $this->error('删除失败');
+        } else {
+            $this->success('删除成功');
         }
     }
 }
