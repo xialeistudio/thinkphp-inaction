15293f023f3a66e56680f4dba0db82fb6a1ac91e
xialeistudio <xialeistudio@gmail.com>
2016-12-08 11:13:30 +0800

    代码重构

--- wechat/Application/Home/Controller/IndexController.class.php
+++ wechat/Application/Home/Controller/IndexController.class.php
@@ -5,14 +5,26 @@ use Think\Controller;
 
 class IndexController extends Controller
 {
-    const REGISTER_STEP_USERNAME = 1;
-    const REGISTER_STEP_PASSWORD = 2;
+    //操作步骤定义
+    const STEP_REGISTER_USERNAME = 1;
+    const STEP_REGISTER_PASSWORD = 2;
+    const STEP_LOGIN_USERNAME = 3;
+    const STEP_LOGIN_PASSWORD = 4;
+    const STEP_AVATAR_UPLOAD = 5;
 
-    const LOGIN_STEP_USERNAME = 3;
-    const LOGIN_STEP_PASSWORD = 4;
+    //游客操作
+    const GUEST_ACTION_REGISTER = 1;
+    const GUEST_ACTION_LOGIN = 2;
+    //用户操作
+    const USER_ACTION_INFO = 1;
+    const USER_ACTION_AVATAR = 2;
+    const USER_ACTION_LOGOUT = 3;
+    //全局操作
+    const GLOBAL_ACTION_RESET = 0;
 
-    const AVATAR_STEP_UPLOAD = 5;
-
+    /**
+     * 外部接口
+     */
     public function index()
     {
         import('Home.Library.ThinkWechat');
@@ -25,7 +37,7 @@ class IndexController extends Controller
     private function _handle(array $data)
     {
         session_id($data['FromUserName']);
-        session_start();
+        session('[start]');
         if ($data['MsgType'] == 'text') {
             return $this->_handleText($data);
         }
@@ -39,133 +51,242 @@ class IndexController extends Controller
     }
 
     /**
-     * 处理文本信息
-     * @param array $data
-     * @return array
+     * 是否游客
+     * @return bool
      */
-    private function _handleText(array $data)
+    private function _isGuest()
     {
-        $step = session('step');
-        //全局操作
-        if ($data['Content'] == '0') {
-            session('step', null);
-            session('login', null);
-            session_destroy();
-            return array('重置成功', 'text');
-        }
-        //未登录
-        if (empty($step) && !session('login')) {
-            if ($data['Content'] == '1') {
-                session('step', self::REGISTER_STEP_USERNAME);
-                return array("【注册】请输入您的账号", 'text');
-            }
-            if ($data['Content'] == '2') {
-                session('step', self::LOGIN_STEP_USERNAME);
-                return array('【登录】请输入您的账号', 'text');
-            }
-        }
-        //已登录
-        if (session('login')) {
-            //查看个人信息
-            if ($data['Content'] == '1') {
-                $avatar = session('avatar');
-                return array(
-                    join("\n", array(
-                        '您的账号:' . session('username'),
-                        '您的密码:' . session('password'),
-                        '您的头像:' . $avatar ? $avatar : '未设置'
-                    )),
-                    'text');
-            }
-            if ($data['Content'] == '2') {
-                session('step', self::AVATAR_STEP_UPLOAD);
-                return array('【头像】请发送头像图片', 'text');
-            }
-            //退出登录
-            if ($data['Content'] == '3') {
-                session('login', null);
-                return array('【注销】您已成功退出登录', 'text');
-            }
-        }
-        //用户注册输入用户名
-        if ($step == self::REGISTER_STEP_USERNAME) {
-            session('username', $data['Content']);
-            session('step', self::REGISTER_STEP_PASSWORD);
-            return array('【注册】请输入您的密码', 'text');
-        }
-        //用户注册输入密码
-        if ($step == self::REGISTER_STEP_PASSWORD) {
-            session('password', $data['Content']);
-            session('step', null);
-            return array('【注册】注册成功', 'text');
-        }
-        //用户登录输入账号
-        if ($step == self::LOGIN_STEP_USERNAME) {
-            if (session('username') != $data['Content']) {
-                return array('【登录】账号不匹配', 'text');
-            }
-            session('step', self::LOGIN_STEP_PASSWORD);
-            return array('【登录】请输入密码', 'text');
-        }
-        //用户登录输入密码
-        if ($step == self::LOGIN_STEP_PASSWORD) {
-            if (session('password') != $data['Content']) {
-                return array('【登录】密码错误', 'text');
-            }
-            session('login', 1);
-            session('step', null);
-            return array(join("\n", array(
-                '登录成功！您可以进行以下操作:',
-                '1.个人信息',
-                '2.上传头像',
-                '3.退出登录'
-            )), 'text');
-        }
+        return session('login') === null;
+    }
+
+    private function _login()
+    {
+        session('login', 1);
+    }
+
+    private function _logout()
+    {
+        session('login', null);
+    }
+
+    /**
+     * 当前操作步骤
+     * @return int
+     */
+    private function _currentStep()
+    {
+        return session('step');
+    }
+
+    /**
+     * 重置步骤
+     */
+    private function _resetStep()
+    {
+        session('step', null);
+    }
+
+    /**
+     * 设置操作步骤
+     * @param $step
+     */
+    private function _setStep($step)
+    {
+        session('step', $step);
+    }
+
+    /**
+     * 重置会话
+     */
+    private function _resetSession()
+    {
+        session('[destroy]');
+    }
+
+
+    /**
+     * 游客操作
+     */
+    private function _guestActions()
+    {
         return array(
-            join("\n", array(
-                '不存在的操作!您可以进行以下操作:',
-                '1.注册账号',
-                '2.登录账号'
-            )),
-            'text'
+            '您当前的身份是【游客】',
+            '您可以进行以下操作：',
+            '1.注册账号',
+            '2.登录账号',
+            '帮助:任何情况下回复0重置会话状态'
         );
     }
 
     /**
-     * 上传头像处理
+     * 用户操作
+     * @return array
+     */
+    private function _userActions()
+    {
+        return array(
+            '您当前的身份是【登录用户】',
+            '您可以进行以下操作',
+            '1.个人信息',
+            '2.上传头像',
+            '3.退出登录',
+            '帮助:任何情况下回复0重置会话状态'
+        );
+    }
+
+    /**
+     * 全局操作
+     * @param array $data
+     * @return array|bool
+     */
+    private function _handleGlobalAction(array $data)
+    {
+        if ($data['Content'] == self::GLOBAL_ACTION_RESET) {
+            $this->_resetSession();
+            return array(join("\n", $this->_guestActions()), 'text');
+        }
+
+        return false;
+    }
+
+    /**
+     * 处理文本信息
+     * @param array $data
+     * @return array|bool
+     */
+    private function _handleText(array $data)
+    {
+        $result = $this->_handleGlobalAction($data);
+        //如果返回非false，证明当前操作已经被处理完成
+        if ($result !== false) {
+            return $result;
+        }
+        //游客
+        if ($this->_isGuest()) {
+            //没有选择任何步骤
+            if (!$this->_currentStep()) {
+                if ($data['Content'] == self::GUEST_ACTION_REGISTER) {
+                    $this->_setStep(self::STEP_REGISTER_USERNAME);
+                    return array(
+                        '【注册】请输入您的用户名',
+                        'text'
+                    );
+                }
+                if ($data['Content'] == self::GUEST_ACTION_LOGIN) {
+                    $this->_setStep(self::STEP_REGISTER_USERNAME);
+                    return array(
+                        '【登录】请输入您的用户名',
+                        'text'
+                    );
+                }
+            }
+            //注册->输入用户名
+            if ($this->_currentStep() == self::STEP_REGISTER_USERNAME) {
+                $this->_setStep(self::STEP_REGISTER_PASSWORD);
+                session('username', $data['Content']);
+                return array('【注册】请输入密码', 'text');
+            }
+            //注册->输入密码
+            if ($this->_currentStep() == self::STEP_REGISTER_PASSWORD) {
+                $this->_resetStep();
+                session('password', $data['Content']);
+                return array(join("\n", array_merge(array('【注册】注册成功'), $this->_guestActions())), 'text');
+            }
+            //登录->输入用户名
+            if ($this->_currentStep() == self::STEP_LOGIN_USERNAME) {
+                if ($data['Content'] != session('username')) {
+                    return array(
+                        join("\n", array(
+                            "【登录】用户名错误",
+                            "回复用户名继续操作",
+                            "回复0重新开始会话"
+                        )),
+                        'text'
+                    );
+                }
+                $this->_setStep(self::STEP_LOGIN_PASSWORD);
+                return array('【登录】请输入密码', 'text');
+            }
+            //登录->输入密码
+            if ($this->_currentStep() == self::STEP_LOGIN_PASSWORD) {
+                if ($data['Content'] != session('password')) {
+                    return array(
+                        join("\n", array(
+                            "【登录】密码错误",
+                            "回复密码继续操作",
+                            "回复0重新开始会话"
+                        )),
+                        'text'
+                    );
+                }
+
+                $this->_login();
+                $this->_resetStep();
+                return array(join("\n", array_merge(array('【登录】登录成功'), $this->_userActions())), 'text');
+            }
+            return array(join("\n", $this->_guestActions()), 'text');
+        } else {
+            if (!$this->_currentStep()) {
+                if ($data['Content'] == self::USER_ACTION_INFO) {
+                    return array(
+                        join("\n", array(
+                            '用户名:' . session('username'),
+                            '密码:' . session('password'),
+                            '头像:' . (session('avatar') ? session('avatar') : '未设置')
+                        )),
+                        'text'
+                    );
+                }
+                if ($data['Content'] == self::USER_ACTION_AVATAR) {
+                    $this->_setStep(self::STEP_AVATAR_UPLOAD);
+                    return array(
+                        '【头像】请上传一张头像',
+                        'text'
+                    );
+                }
+                if ($data['Content'] == self::USER_ACTION_LOGOUT) {
+                    $this->_login();
+                    $this->_resetStep();
+                    return array(join("\n", array_merge(array('退出登录成功'), $this->_guestActions())), 'text');
+                }
+            }
+            if ($this->_currentStep() == self::USER_ACTION_AVATAR) {
+                return array(
+                    '【头像】操作有误!请上传图片头像',
+                    'text'
+                );
+            }
+            return array(join("\n", $this->_userActions()), 'text');
+        }
+    }
+
+    /**
+     * 处理图片消息
      * @param array $data
      * @return array
      */
     private function _handleImage(array $data)
     {
-        if (session('login') && session('step') == self::AVATAR_STEP_UPLOAD) {
-            session('avatar', $data['PicUrl']);
-            session('step', null);
-            return array('【头像】设置成功', 'text');
+        if ($this->_currentStep() != self::STEP_AVATAR_UPLOAD) {
+            $messages = array('操作有误');
+            if ($this->_isGuest()) {
+                $messages = array_merge($messages, $this->_guestActions());
+            } else {
+                $messages = array_merge($messages, $this->_userActions());
+            }
+            return array(join("\n", $messages), 'text');
         }
-        return array(
-            join("\n", array(
-                '不存在的操作!您可以进行以下操作:',
-                '1.注册账号',
-                '2.登录账号'
-            )),
-            'text'
-        );
+
+        session('avatar', $data['PicUrl']);
+        return array(join("\n", array_merge(array('【头像】上传成功'), $this->_userActions())), 'text');
     }
 
     private function _handleEvent(array $data)
     {
         if ($data['Event'] == 'subscribe') {
-            return array(
-                join("\n", array(
-                    '欢迎关注!您可以进行以下操作',
-                    '1.注册账号',
-                    '2.登录账号'
-                )),
-                'text'
-            );
+            return array(join("\n", array_merge(array('欢迎关注！'), $this->_guestActions())), 'text');
         }
-        return array('你好', 'text');
+        return array('操作有误', 'text');
     }
 
     /**
